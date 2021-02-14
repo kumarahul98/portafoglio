824d47658258e73fb1d12583046f24c6f6770fcd
indr199 <indravesh@d3vtech.com>
2021-02-15 02:34:04 +0530

    work in progress

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import work from "./images/workInProgress.gif";
+import "./App.css";
+import { AiOutlineTwitter } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+import { RiGithubLine } from "react-icons/ri";
+import { AiOutlineInstagram } from "react-icons/ai";
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <img src={work} id="work" className="App-logo" alt="logo" />
+        <h2>Reach me out at: </h2>
+        <div className="sc">
+        <a href="https://twitter.com/kuma_r_ahul">
+          <AiOutlineTwitter id="twitter" className="icon" />
         </a>
+        <a href="https://www.linkedin.com/in/kumarahul98/">
+          <FaLinkedinIn id="linkedIn" className="icon" />
+        </a>
+        <a href="https://github.com/kumarahul98">
+          <RiGithubLine id="gitHub" className="icon" />
+        </a>
+        <a href="https://www.instagram.com/kumarahul98/">
+          <AiOutlineInstagram id="intagram" className="icon" />
+        </a>
+        <a href="https://wa.me/7019046050">
+          <AiOutlineWhatsApp id="whatsApp" className="icon" />
+        </a></div>
       </header>
     </div>
   );
