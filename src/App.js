14352a22ca076e6e5049df2e0e9ccc9817b84c02
kumarahul98 <kumar.rahul98k@gmail.com>
2021-02-15 15:03:49 +0530

    Meanwhile

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
-import work from "./images/workInProgress.gif";
-import "./App.css";
-import { AiOutlineTwitter } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-import { RiGithubLine } from "react-icons/ri";
-import { AiOutlineInstagram } from "react-icons/ai";
-import { AiOutlineWhatsApp } from "react-icons/ai";
+import work from './images/workInProgress.gif';
+import './App.css';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
+import { RiGithubLine } from 'react-icons/ri';
+import { AiOutlineInstagram } from 'react-icons/ai';
+import { AiOutlineWhatsApp } from 'react-icons/ai';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={work} id="work" className="App-logo" alt="logo" />
-        <h2>Reach me out at: </h2>
-        <div className="sc">
-        <a href="https://twitter.com/kuma_r_ahul">
-          <AiOutlineTwitter id="twitter" className="icon" />
-        </a>
-        <a href="https://www.linkedin.com/in/kumarahul98/">
-          <FaLinkedinIn id="linkedIn" className="icon" />
-        </a>
-        <a href="https://github.com/kumarahul98">
-          <RiGithubLine id="gitHub" className="icon" />
-        </a>
-        <a href="https://www.instagram.com/kumarahul98/">
-          <AiOutlineInstagram id="intagram" className="icon" />
-        </a>
-        <a href="https://wa.me/+917019046050">
-          <AiOutlineWhatsApp id="whatsApp" className="icon" />
-        </a></div>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={work} id="work" className="App-logo" alt="logo" />
+				<h2>Meanwhile reach me out at: </h2>
+				<div className="sc">
+					<a href="https://twitter.com/kuma_r_ahul">
+						<AiOutlineTwitter id="twitter" className="icon" />
+					</a>
+					<a href="https://www.linkedin.com/in/kumarahul98/">
+						<FaLinkedinIn id="linkedIn" className="icon" />
+					</a>
+					<a href="https://github.com/kumarahul98">
+						<RiGithubLine id="gitHub" className="icon" />
+					</a>
+					<a href="https://www.instagram.com/kumarahul98/">
+						<AiOutlineInstagram id="intagram" className="icon" />
+					</a>
+					<a href="https://wa.me/+917019046050">
+						<AiOutlineWhatsApp id="whatsApp" className="icon" />
+					</a>
+				</div>
+			</header>
+		</div>
+	);
 }
 
 export default App;
