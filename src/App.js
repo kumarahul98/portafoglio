9de7dce0bdeb5da025c8790c29dfef39b93ba5c8
kumarahul98 <kumar.rahul98k@gmail.com>
2021-02-16 15:42:25 +0530

    added _blank

--- src/App.js
+++ src/App.js
@@ -13,19 +13,39 @@ function App() {
 				<img src={work} id="work" className="App-logo" alt="Work In Progress" />
 				<h2>Meanwhile reach me out at: </h2>
 				<div className="sc">
-					<a aria-label="Twitter" href="https://twitter.com/kuma_r_ahul">
+					<a
+						target="_blank"
+						aria-label="Twitter"
+						href="https://twitter.com/kuma_r_ahul"
+					>
 						<AiOutlineTwitter id="twitter" className="icon" />
 					</a>
-					<a aria-label="linkedIn" href="https://www.linkedin.com/in/kumarahul98/">
+					<a
+						target="_blank"
+						aria-label="linkedIn"
+						href="https://www.linkedin.com/in/kumarahul98/"
+					>
 						<FaLinkedinIn id="linkedIn" className="icon" />
 					</a>
-					<a aria-label="Github" href="https://github.com/kumarahul98">
+					<a
+						target="_blank"
+						aria-label="Github"
+						href="https://github.com/kumarahul98"
+					>
 						<RiGithubLine id="gitHub" className="icon" />
 					</a>
-					<a aria-label="Instagram" href="https://www.instagram.com/kumarahul98/">
+					<a
+						target="_blank"
+						aria-label="Instagram"
+						href="https://www.instagram.com/kumarahul98/"
+					>
 						<AiOutlineInstagram id="intagram" className="icon" />
 					</a>
-					<a aria-label="WhatsApp" href="https://wa.me/+917019046050">
+					<a
+						target="_blank"
+						aria-label="WhatsApp"
+						href="https://wa.me/+917019046050"
+					>
 						<AiOutlineWhatsApp id="whatsApp" className="icon" />
 					</a>
 				</div>
